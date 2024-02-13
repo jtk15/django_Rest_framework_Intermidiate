ef804f296e9a87fa7b5dd9434e55f4d7fe22d07f
jtk15 <joeltonportopl15@gmail.com>
2024-02-13 09:36:42 -0300

    Foi implementado os modelos de dados da aplicação.

--- school/models.py
+++ school/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Base(models.Model):
+    
+    criation = models.DateTimeField(auto_now_add=True)
+    update = models.DateField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        
+        abstract = True
+
+class Course(Base):
+    
+    title = models.CharField('Titulo', max_length=100)
+    url = models.CharField(max_length=200)
+    
+    class Meta:
+        
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
+
+class Assessment(Base):
+     
+    course = models.ForeignKey(Course, related_name='assessments', on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254)
+    score = models.DecimalField(max_digits=5, decimal_places=2)
+    comment = models.TextField()
+    
+    class Meta:
+    
+        verbose_name = 'Avaliação'
+        verbose_name_plural = 'Avaliações'
+        unique_together = ['email', 'course']
